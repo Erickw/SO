9313ea41b3ffad90f4389efe9017ec6ab2415cbd
Erick Machado <erickmachado@MacBook-Pro-de-Erick.local>
2019-08-26 18:07:14 -0300

    remove schedule library

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-import os, signal, sys, psutil, time, schedule
+import os, signal, sys, psutil, time
 from threading import Thread
 
 
@@ -8,26 +8,25 @@ def get_process_list():
 
 
 def child():
-    created_processes.append(os.getpid())
-    print('\n############################## New Child Created %d ##############################' % os.getpid())
+    print('                   \n############################## New Child Created %d ##############################                   ' % os.getpid())
 
 def create_process():
     newpid = os.fork()
     if newpid == 0:
         child()
     else:
-        print("############################## New Parent Created: ############################## " , os.getpid())
+        print("                   \n############################## New Parent Created %d ##############################                   " , os.getpid())
 
 def process_manager():
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        time.sleep(10)
+        create_process()
+        get_process_list()
 
 def main():
     get_process_list()
     while True:
-        print os.getpid()
-        reply = raw_input("Do you want create more process? Insert y for yes, v to view current process pid or k to kill a specific process\n")
+        reply = raw_input("========================================= Do you want create more process? Insert y for yes, v to view current process pid or k to kill a specific process =========================================\n")
 
         if reply == 'y':
             create_process()
@@ -36,16 +35,12 @@ def main():
         elif reply == 'k':
             pid = raw_input("Insert the pid process you want to kill\n")
             print(int(pid))
-            os.kill(os.getppid(), signal.SIGKILL)
+            os.kill(os.getppid(), signal.SIGTERM)
             print("########################################## The process", pid, "was killed ##########################################")
             get_process_list()
 
         elif reply == 'v':
             print(os.getpid())
-
-
-
-schedule.every(10).seconds.do(create_process)
 
 thread1 = Thread(target=main,args=[])
 thread2 = Thread(target=process_manager,args=[])
